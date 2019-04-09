5509996b8f557d30795783f43252a475ac8463f6
danim219 <48729441+danim219@users.noreply.github.com>
2019-04-09 19:12:01 +0300

    aa

---
+++ Sedinta05/ex01/app.js
@@ -0,0 +1,9 @@
+
+
+console.log(
+    (new Date()).getHours() + ':' +
+    (new Date()).getMinutes() + ':' +
+    (new Date()).getSeconds() + ':' +
+    (new Date()).getMilliseconds(),
+    'Am ajuns la Pixellab.'
+);
