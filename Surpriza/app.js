511d425630ac3faffd8dd580d7559c4def952f49
danim219 <48729441+danim219@users.noreply.github.com>
2019-04-11 19:12:27 +0300

    new

---
+++ Surpriza/app.js
@@ -0,0 +1,50 @@
+var inputlimbasiLiteraturaRomana = document.getElementById('limbasiLiteraturaRomana'),
+    inputfranceza = document.getElementById('franceza'),
+    inputengleza = document.getElementById('engleza'),
+    inputmatematica = document.getElementById('matematica'),
+    inputfizica = document.getElementById('fizica'),
+    inputchimie = document.getElementById('chimie'),
+    inputbiologie = document.getElementById('biologie'),
+    inputistoria = document.getElementById('istoria'),
+    inputgeografia = document.getElementById('geografia'),
+    inputstiinteSocioUmane = document.getElementById('stiinteSocioUmane'),
+    inputreligie = document.getElementById('religie'),
+    inputeducatieMuzicala = document.getElementById('educatieMuzicala'),
+    inputeducatiePlastica = document.getElementById('educatiePlastica'),
+    inputsport = document.getElementById('sport'),
+    inputinformatica = document.getElementById('informatica'),
+    inputtic = document.getElementById('tic'),
+    inputantreprenoriala = document.getElementById('antreprenoriala'),
+
+    elementResult = document.getElementById('result'),
+    form = document.querySelector('form');
+
+    form.addEventListener('submit', function(e) {
+      var limbasiLiteraturaRomana = inputlimbasiLiteraturaRomana.value || 0,
+      franceza = inputfranceza.value || 0,
+      engleza = inputengleza.value || 0,
+      matematica = inputmatematica.value || 0,
+      fizica = inputfizica.value || 0,
+      chimie = inputchimie.value || 0,
+      biologie = inputbiologie.value || 0,
+      istoria = inputistoria.value || 0,
+      geografia = inputgeografia.value || 0,
+      stiinteSocioUmane = inputstiinteSocioUmane.value || 0,
+      religie = inputreligie.value || 0,
+      educatieMuzicala = inputeducatieMuzicala.value || 0,
+      educatiePlastica = inputeducatiePlastica.value || 0,
+      sport = inputsport.value || 0,
+      informatica = inputinformatica.value || 0,
+      tic = inputtic.value || 0,
+      antreprenoriala = inputantreprenoriala.value || 0,
+
+      result = 0;
+      result = ((parseFloat(limbasiLiteraturaRomana) + parseFloat(franceza) + parseFloat(engleza) + parseFloat(matematica) + parseFloat(fizica) + parseFloat(chimie) + parseFloat(biologie) + parseFloat(istoria) + parseFloat(geografia) + parseFloat(stiinteSocioUmane) + parseFloat(religie) + parseFloat(educatieMuzicala) +parseFloat(educatiePlastica) + parseFloat(sport) + parseFloat(informatica) + parseFloat(tic) + parseFloat(antreprenoriala)) / 17).toFixed(2);
+
+
+      elementResult.innerText =result;
+
+      if (result > 8.5)
+      alert ('Felicitari, ati obtinut medie de buresa!')
+      e.preventDefault();
+    }, false);
