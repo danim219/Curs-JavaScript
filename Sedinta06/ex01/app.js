cd215196d3d2cbf9c2ecfdd448ccd19e3664f120
danim219 <48729441+danim219@users.noreply.github.com>
2019-04-16 21:57:26 +0300

    hb

--- Sedinta06/ex01/app.js
+++ Sedinta06/ex01/app.js
@@ -1,20 +1,36 @@
 var car = {
-    make: 'Alfa-Romeo',
-    color: 'red',
-    wheels: 4,
-    speed: 120,
-    topSpeed: 280,
-    topReverseSpeed: -70,
+        make: 'Alfa-Romeo Giulia',
+        color: 'red',
+        wheels: 4,
+        speed: 120,
+        topSpeed: 280,
+        topReverseSpeed: -70,
     accelerate: function() {
         this.speed++;   
     },
     decelerate: function() {
         this.speed--;
     },
+    stop: function() {
+        speed = 0;
+    },
+    setSpeed: function (s) {
+        if ( s > car.topSpeed){
+        car.speed = car.topSpeed;
+    }
+        if ( s < car.topReverseSpeed){
+        car.speed = car.topReverseSpeed;
+        }
+    }
 };
 
-console.log('Masina era marca ' +  car.make + ' si se deplaseaza cu ' + car.speed + ' km/h.')
-console.log('')
-spd = car.speed - 5;
-console.log('Viteza noua este ' + spd + ' km/h.')
-console.log('')
+console.log('Masina era marca ' +  car.make + ' si se deplaseaza cu ' + car.speed + ' km/h.');
+console.log('');
+
+var i = 0;
+for(;i < 5 ; i++ ){ 
+    car.decelerate();
+} 
+console.log('Viteza noua este ' + car.speed + ' km/h.');
+console.log('');
+
