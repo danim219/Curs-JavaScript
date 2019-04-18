7eaf9e3ef68db3717738bed523b9b4c2788db29e
danim219 <48729441+danim219@users.noreply.github.com>
2019-04-18 22:05:44 +0300

    xdfaxs

--- Sedinta06/ex05/app.js
+++ Sedinta06/ex05/app.js
@@ -1,23 +1,31 @@
+
 (function() {
-    document.addEventListener('DOMContentLoaded', function() {
-      var showButton = document.querySelector('#showButton'),
-        toggleEventButton = document.querySelector('#toggleEventButton'),
+  document.addEventListener('DOMContentLoaded', function() {
+    var showButton = document.querySelector('#showButton'),
+      toggleEventButton = document.querySelector('#toggleEventButton'),
+      paragraph = document.createElement('p');
+      eventBound = true;
+
+    function showAlert() {
+      alert('Butonul a fost apasat');
+    }
+
+    showButton.addEventListener('click', showAlert);
+    paragraph.innerText = 'Alerta va fi afisata';
+    document.body.appendChild(paragraph);
+
+    toggleEventButton.addEventListener('click', function() {
+      if (eventBound) {
+        showButton.removeEventListener('click', showAlert);
+        this.innerText = 'Porneste afisarea.';
+        paragraph.innerText = 'Alerta nu va fi afisata';
+        eventBound = false;
+      } else {
+        showButton.addEventListener('click', showAlert);
+        this.innerText = 'Opreste afisare.';
+        paragraph.innerText = 'Alerta va fi afisata';
         eventBound = true;
-  
-      function showAlert() {
-        alert('Butonul a fost apasat');
       }
-  
-      showButton.addEventListener('click', showAlert);
-  
-      toggleEventButton.addEventListener('click', function() {
-        if (eventBound) {
-          showButton.removeEventListener('click', showAlert);
-          eventBound = false;
-        } else {
-          showButton.addEventListener('click', showAlert);
-          eventBound = true;
-        }
-      });
     });
-  })();
+  });
+})();
