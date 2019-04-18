fe3113601de27a47ab4a87758899da582149313a
danim219 <48729441+danim219@users.noreply.github.com>
2019-04-18 21:46:05 +0300

    asa

--- Sedinta06/ex05/app.js
+++ Sedinta06/ex05/app.js
@@ -1,5 +1,23 @@
 (function() {
-  document.addEventListener('DOMContentLoaded', function() {
-    
-  });
-})();
+    document.addEventListener('DOMContentLoaded', function() {
+      var showButton = document.querySelector('#showButton'),
+        toggleEventButton = document.querySelector('#toggleEventButton'),
+        eventBound = true;
+  
+      function showAlert() {
+        alert('Butonul a fost apasat');
+      }
+  
+      showButton.addEventListener('click', showAlert);
+  
+      toggleEventButton.addEventListener('click', function() {
+        if (eventBound) {
+          showButton.removeEventListener('click', showAlert);
+          eventBound = false;
+        } else {
+          showButton.addEventListener('click', showAlert);
+          eventBound = true;
+        }
+      });
+    });
+  })();
